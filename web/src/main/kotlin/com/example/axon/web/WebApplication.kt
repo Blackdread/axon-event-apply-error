61f160a227de826104fb2215f88dbf6d23c2413d
Yoann CAPLAIN <caplain.yoann@gmail.com>
2023-04-18 21:24:34 +0200

    impl

--- web/src/main/kotlin/com/example/axon/web/WebApplication.kt
+++ web/src/main/kotlin/com/example/axon/web/WebApplication.kt
@@ -1,14 +1,44 @@
 package com.example.axon.web
 
+import com.example.axon.common.*
+import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @SpringBootApplication(
-	exclude = [DataSourceAutoConfiguration::class]
+    exclude = [DataSourceAutoConfiguration::class]
 )
 class WebApplication
 
 fun main(args: Array<String>) {
-	runApplication<WebApplication>(*args)
+    runApplication<WebApplication>(*args)
+}
+
+@RestController
+class MyController(
+    private val reactorCommandGateway: ReactorCommandGateway,
+) {
+
+    val myEntityOtherId = MyEntityOtherId.myEntityOtherId()
+
+    @GetMapping("/api/create-root")
+    fun createRoot(): Mono<RootId> {
+        return reactorCommandGateway.send(RootCreateCmd("a"))
+    }
+
+    @GetMapping("/api/create-my-entity")
+    fun createRootMyEntity(): Mono<Unit> {
+        return reactorCommandGateway.send(RootMyEntityCreateCmd(MyEntityType.OTHER1))
+    }
+
+    @GetMapping("/api/create-my-entity-level/{level}")
+    fun addRootMyEntityOther(@PathVariable level: Int): Mono<Unit> {
+        return reactorCommandGateway.send(RootMyEntityOtherAddCmd(MyEntityType.OTHER1, myEntityOtherId, level))
+    }
+
 }
